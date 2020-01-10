f3c3c424ef936f80158df9bb10f00d6ebe7ef7e8
jeremypng <jsanders@cspire.com>
2020-01-10 12:10:05 -0500

    parameter table/decoding

--- lib/serial/impl.ex
+++ lib/serial/impl.ex
@@ -21,7 +21,7 @@ defmodule Serial.Impl do
   end
 
   def get_parameter(pid, param) do
-    param_id = OBD2.Parameters.get_param_by_atom(param)
+    %{id: param_id} = OBD2.Parameters.get_param_by_atom(param)
     cs = 25 + :binary.decode_unsigned(param_id)
     Circuits.UART.write(pid,<<0x01,0x01,0x22,0x01,param_id,cs>>)
   end
