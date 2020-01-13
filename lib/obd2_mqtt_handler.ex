c2f10b102237137827a7fdf691e4f072e6345730
jeremypng <jsanders@cspire.com>
2020-01-13 15:42:11 -0600

    mqtt

--- lib/obd2_mqtt_handler.ex
+++ lib/obd2_mqtt_handler.ex
@@ -52,7 +52,7 @@ defmodule OBD2.MQTT.Handler do
     IO.inspect("command_request")
     Logger.info("#obd2/command_requests #{inspect(publish)}")
     # IO.inspect(Jason.decode!(publish,[{'keys', 'atoms'}]))
-    %{command: cmd} = Jason.decode!(publish,[{'keys', 'atoms'}])
+    %{command: cmd} = Jason.decode!(publish,[{:keys, :atoms}])
     case cmd do
       "redetect_vehicle" -> Serial.redetect_vehicle
     end
