e31172a9bc08d6c0744f1009d62e4b24e1790c87
jeremypng <jsanders@cspire.com>
2020-01-13 20:51:23 -0600

    OTP config

--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,7 @@
 import Config
 
-mqtt_server = System.fetch_env!("OBD2_MQTT_SERVER")
-serial_port = System.fetch_env!("OBD2_SERIAL_PORT")
+mqtt_server = System.get_env("OBD2_MQTT_SERVER","10.4.200.4")
+serial_port = System.get_env("OBD2_SERIAL_PORT","cu.usbserial")
 
 config :obd2_streamer,
   mqtt_server: mqtt_server,
